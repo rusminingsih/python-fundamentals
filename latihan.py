aa9b418dc3fc50b6a825217ab587fcd6bd74fb63
Rusmi Ningsih <sehnngseh@gmail.com>
2026-07-11 15:35:07 +0700

    latihan dasar python

---
+++ latihan.py
@@ -0,0 +1,12 @@
+print ("Hallo Ningsih")
+print ("Selamat datang di program latihan.py")
+
+# variabel 
+nama = "Ningsih"
+umur = 18
+jurusan = "Teknik Informasi "
+
+print (nama)
+print (umur)
+print (jurusan)
+print (type(umur))
