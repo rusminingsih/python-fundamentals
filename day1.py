c1fb55202e2bb4c7a1233f936d7627e4fa768893
Rusmi Ningsih <sehnngseh@gmail.com>
2026-07-12 09:48:58 +0700

    day 1 latihan python dasar

---
+++ day1.py
@@ -0,0 +1,10 @@
+nama = "Ningsih"
+tempat_lahir = "Magetan"
+tanggal_lahir = "18 Juli 2007"
+umur = 18
+prodi = "Teknik Informasi"
+
+print (f"Hallo nama saya {nama}")
+print (f"Saya lahir di {tempat_lahir} pada {tanggal_lahir}")
+print (f"Saya berumur {umur} tahun")
+print (f"Saya kuliah di program studi {prodi}")
