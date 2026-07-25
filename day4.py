475135cac31854a4da2f2d953cafcacd3335c22a
Rusmi Ningsih <sehnngseh@gmail.com>
2026-07-25 20:27:31 +0700

    day 2 input data

---
+++ day4.py
@@ -0,0 +1,6 @@
+nama = str(input("Masukkan nama Anda :"))
+umur = int(input("Masukkan umur Anda :"))
+jurusan = str(input("Masukkan jurusan Anda :"))
+makanan_favorit = str(input("Masukkan makanan favorit Anda :"))
+
+print (f"Hallo {nama} umur kamu {umur} tahun, jurusan yang kamu ambil {jurusan} dan makanan_favoritmu adalah {makanan_favorit}")
