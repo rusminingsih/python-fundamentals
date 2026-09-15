42f8a768a87366bbb1069d19330db0e5afce3628
Rusmi Ningsih <sehnngseh@gmail.com>
2026-09-15 21:04:45 +0700

    fix: perbaiki kelompok13_heaven_bryan_adams.py

--- kelompok13_heaven_bryan_adams.py
+++ kelompok13_heaven_bryan_adams.py
@@ -10,15 +10,15 @@ def lirik():
 
         ("Now nothin' can take you away from me", 0.1),
         ("We've been down that road before - but that's over now", 0.1),
-        ("You keep me comin' back for more", 0.1)
+        ("You keep me comin' back for more", 0.1),
 
-        ("Baby you're all that I want - when you're lyin' here in my arms", 0.1),
+        ("Baby you're all that I want - when you're lyin' here in my arms", 0.2),
         ("i'm findin it hard to believe - we're in heaven", 0.1),
         ("And love is all that I need and I found it there in your heart", 0.1),
         ("It isn't to hard to see - we're in heaven", 0.1),
     ]
 
-    delay = [2, 3, 2.5, 0.1, 0.1, 1, 2, 3, 2.5, 0.1, 0.1, 1, 2, 3, 2.5, 0.1, 0.1]
+    delay = [2, 3, 2.5, 0.1, 0.1, 1, 3, 3, 2.5, 0.1]
 
     print("\nHeaven - Bryan Adams\n")
     time.sleep(2)
